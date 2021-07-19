7549ebb9940e95d0554badbaafb33433736b5a37
HuanghaiCH <970542951@qq.com>
2021-07-19 19:19:30 +0800

    web_project modify by huanghai date 2021-07-10 19:16

---
+++ app.py
@@ -0,0 +1,17 @@
+import logging;logging.basicConfig(level=logging.INFO)
+import asyncio
+from aiohttp import web
+
+def dealpost(request):
+    return web.Response(body=b'<h1>home</h1>', content_type='text/html')
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', dealpost)
+    srv = await loop.create_server(app.make_handler(),'127.0.0.1', 39000)
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
---
+++ ocm.py
@@ -0,0 +1,19 @@
+import asyncio
+import aiomysql
+import logging;logging.basicConfig(level=logging.INFO)
+
+async def create_pool(loop, **kwargs):
+    logging.INFO('create database connect pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kwargs.get('host', 'localhost'),
+        port=kwargs.get('port', 3306),
+        user=kwargs['user'],
+        password=kwargs['password'],
+        db=kwargs['db'],
+        charset=kwargs.get('charset', 'utf8'),
+        autocommit=kwargs.get('autocommit', True),
+        maxsize=kwargs.get('maxsize', 10),
+        minsize=kwargs.get('minsize', 1),
+        loop=loop
+    )
